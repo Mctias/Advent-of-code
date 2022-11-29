f062d6b11770a2d6b1a042d3564b06f55559f62c
Mattias Persson <mattias.persson@skf.com>
2022-11-29 08:56:28 +0100

    Set up Java project and file reader for 2022

---
+++ 2022/Java/Advent of Code 2022/src/Day1.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Day1 {
+    Reader fileReader = new Reader();
+    ArrayList<Integer> input = fileReader.lineByLine("day1");
+
+    private int part1 = 0;
+    private  int part2 = 0;
+
+    public void printResults(){
+        solvePart1();
+        System.out.println("Day 1a: " + part1);
+        solvePart2();
+        System.out.println("Day 1b: " + part2);
+    }
+
+    public void solvePart1(){
+
+        int numberOfIncreases = 0;
+        int lastNumber = Integer.MAX_VALUE;
+        for(int i = 0; i < input.size(); i++){
+            if(input.get(i) > lastNumber){
+                numberOfIncreases++;
+            }
+            lastNumber = input.get(i);
+        }
+        part1 = numberOfIncreases;
+    }
+
+    public void solvePart2(){
+        int numberOfIncreases = 0;
+        ArrayList<Integer> sums = new ArrayList<Integer>();
+        for(int i = 0; i < input.size() - 2; i++){
+            int tot = input.get(i) + input.get(i + 1) + input.get(i + 2);
+            sums.add(tot);
+        }
+
+        int lastNumber = Integer.MAX_VALUE;
+        for(int i = 0; i < sums.size(); i++){
+            if(sums.get(i) > lastNumber){
+                numberOfIncreases++;
+            }
+            lastNumber = sums.get(i);
+        }
+        part2 = numberOfIncreases;
+    }
+
+
+}
---
+++ 2022/Java/Advent of Code 2022/src/Reader.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Reader {
+    static final String DATA_PATH = "C:\\Users\\Mattias Persson\\Documents\\Jobb\\AoC\\2022\\Java\\Advent of Code 2022\\data\\";
+    /*
+       1
+       2
+       3
+     */
+    public ArrayList<Integer> lineByLine(String day) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        try{
+            String filePath = DATA_PATH + day + ".txt";
+            File file = new File(filePath);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+
+            while((st = br.readLine()) != null){
+                result.add(Integer.parseInt(st));
+            }
+            br.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found\n" + e);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Failed to format string to number\n" + e);
+        }
+        catch (Exception e) {
+            System.out.println("Something broke :)\n" + e);
+        }
+        return result;
+    }
+
+    /*
+      1,2,3
+     */
+    public ArrayList<Integer> commaSeparated(String day) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        try{
+            String filePath = DATA_PATH + day + ".txt";
+            File file = new File(filePath);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+
+            while((st = br.readLine()) != null){
+                String[] tokens = st.split(",");
+                for (String s : tokens){
+                    result.add(Integer.parseInt(s));
+                }
+            }
+
+            br.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found\n" + e);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Failed to format string to number\n" + e);
+        }
+        catch (Exception e) {
+            System.out.println("Something broke :)\n" + e);
+        }
+        return result;
+    }
+}
